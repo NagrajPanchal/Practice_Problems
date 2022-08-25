da82591274a5b89d3c4d1c1596456f72d5ea11db
Nagraj <nagrajsubhashpanchal.gmail.com>
2022-08-25 12:11:28 +0530

    Add : The Default value of Primitive data types

---
+++ src/Default_Value_Primitive_Data_Types.java
@@ -0,0 +1,24 @@
+public class Default_Value_Primitive_Data_Types
+{
+byte b;
+short s;
+int i;
+long l;
+float f;
+double d;
+boolean flag;
+char ch;
+
+    public static void main(String[] args)
+    {
+        Default_Value_Primitive_Data_Types obj = new Default_Value_Primitive_Data_Types();
+        System.out.println("The Default value of byte is : " +obj.b);
+        System.out.println("The Default value of short is : " +obj.s);
+        System.out.println("The Default value of integer is : " +obj.i);
+        System.out.println("The Default value of Long is : " +obj.l);
+        System.out.println("The Default value of float is : " +obj.f);
+        System.out.println("The Default value of double is : " +obj.d);
+        System.out.println("The Default value of boolean is : " +obj.flag);
+        System.out.println("The Default value of Char is : " +obj.ch);
+    }
+}
